05e4aa47ffc245450f6544a377ae5e9545c4fbd4
Atamert Can <atamertcan@Atamert-MacBook-Air.local>
2025-08-24 18:24:35 +0300

    updated

---
+++ 8keypoint_ve_ozellik_cikarimi.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+import numpy as np
+
+#Keypoints bulmak için tanımlamak için kullanılan bir algoritma. Görüntü eşleştirme, nesne tanıma gibi alanlarda kullanılır
+#Görsel Gaussian blur ile farklı derecelerde bulanıklaştırılır ve aralarındaki farklar alınır(DoG).
+#Her piksel komşu piksellerle karşılaştırılır ve buna göre keypointler belirlenir.
+
+img = cv.imread("Photos/character.jpg", 0)
+
+sift = cv.SIFT_create()
+#keypoints=sift.detect(img, None)
+keypoints, des = sift.detectAndCompute(img, None)
+img1 = cv.drawKeypoints(img, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+#img2 = cv.drawKeypoints(img, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_DEFAULT)
+#img3 = cv.drawKeypoints(img, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+
+
+#eşleştirme için kullanılıyor 
+print("Keypoint sayısı:", len(keypoints))
+print("Descriptor boyutu:", des.shape)
+print("İlk keypoint'in descriptor'u:\n", des[0])
+
+
+cv.imshow("comparison", np.hstack([img1]))
+
+cv.waitKey(0)
+cv.destroyAllWindows()
---
+++ 8keypoint_ve_ozellik_cikarimi2.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+#Kullanım olarak ve genel mantık olarak SIFT ile benzer. SIFT'e göre çok daha hızlı.
+
+img = cv.imread("Photos/character.jpg", cv.IMREAD_GRAYSCALE)
+
+orb = cv.ORB_create()
+keypoints = orb.detect(img, None)
+keypoints,_ = orb.compute(img, keypoints)#descriptor için
+img = cv.drawKeypoints(img,keypoints,img, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+cv.imshow("keypoints", img)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
+
