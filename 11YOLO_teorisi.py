094625d799df1168433046a8749bcbdf4bb7d8cc
Atamert Can <atamertcan@Atamert-MacBook-Air.local>
2025-08-26 19:39:28 +0300

    YOLO updated

--- 11YOLO_teorisi.py
+++ 11YOLO_teorisi.py
@@ -6,11 +6,22 @@ ve ızgaradan sonra anchorlu yapıya geçerken daha ileride anchor free(YOLOX) h
 YOLO R modeliyle ise multi-task learning kullanılmıştırç
 
 """
-
 from ultralytics import YOLO
 import cv2 as cv
 
 model = YOLO("YOLO_weights/yolov8n.pt")
-result=model("Photos/cat.jpeg",show=True)
+img = cv.imread("Photos/cars.jpg")
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+img1 = cv.GaussianBlur(img,(5,5),0)
+img2 = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,15,5)
+img2gray = cv.cvtColor(img2,cv.COLOR_GRAY2BGR)
+
+result1=model(img2gray)
+result2=model(img1)
+result3=model("Photos/cars.jpg")
+
+cv.imshow("Threshold", result1[0].plot())
+cv.imshow("Blur", result2[0].plot())
+cv.imshow("Original", result3[0].plot())
 
 cv.waitKey(0)
